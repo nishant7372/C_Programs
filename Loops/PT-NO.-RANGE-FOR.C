7ea3cf690af12fb483da5830d2836699243641b7
NishantSingh02 <nst7372@gmail.com>
2022-12-31 23:55:15 +0530

    i

---
+++ Loops/PT-NO.-RANGE-FOR.C
@@ -0,0 +1,14 @@
+/*Write a progarm to print numbers of range 1-100 using for loop*/
+
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+ int i;
+ clrscr();
+ for(i=1;i<=100;i++)
+ {
+ printf("%d \t",i);
+ }
+ getch();
+ }
