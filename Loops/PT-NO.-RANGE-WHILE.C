b27544e62570ce5029f1bdc583c67749f7524076
NishantSingh02 <nst7372@gmail.com>
2022-12-31 23:56:16 +0530

    i

---
+++ Loops/PT-NO.-RANGE-WHILE.C
@@ -0,0 +1,15 @@
+/*Write a program to print no. from the range 1-100 using while loop.*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=1;
+clrscr();
+while(i<=5)
+{
+printf("%d \n",i);
+i++;
+}
+getch();
+}
